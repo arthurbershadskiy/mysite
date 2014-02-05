8df250fe4e8f9f55ceac348f3fe0869979c90a95
Arthur Bershadskiy <arthurbershadskiy@gmail.com>
2014-02-04 21:10:35 -0500

    Add about me section.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Mysite::Application.routes.draw do
       
   root "pages#home"
-  
+  get '/contact', to:'pages#contact', as: 'contact'
+  get '/about', to:'pages#about', as: 'about'
   get "/home", to: "pages#home", as: "home"
 end
